1fd93b3ce1028859a2631c293450d9bccf936b75
sweettime111 <30824785+sweettime111@users.noreply.github.com>
2018-04-19 14:52:10 +0700

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { MyPage } from './../my/my';
+
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  gotoMyPage(){
+    this.navCtrl.push(MyPage);
+
+}
+
+
+}
